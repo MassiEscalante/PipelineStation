07f7db8b47605c3cbe5e5f62f720be7637fd660e
MassiEscalante <massiel2611@gmail.com>
2024-11-17 16:46:10 -0800

    updated for test

--- client/cypress.config.js
+++ client/cypress.config.js
@@ -1,14 +1,15 @@
-import { defineConfig } from 'cypress';
+import { defineConfig } from "cypress";
 
 export default defineConfig({
   component: {
     devServer: {
-      framework: 'react',
-      bundler: 'vite',
+      framework: "react",
+      bundler: "vite",
     },
-    specPattern: 'cypress/component/**/*.cy.{js,jsx,ts,tsx}',
+    specPattern: "cypress/component/**/*.cy.{js,jsx,ts,tsx}",
   },
   e2e: {
-    supportFile: 'cypress/support/e2e.js', 
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    supportFile: "cypress/support/e2e.js", 
   },
 });
